c19ab945d236f8296642035a8eeba581bd7a2999
ahmedtaaw <ahmedtaaw@gmail.com>
2019-07-22 04:21:36 +0400

    way to get state

---
+++ study/index.js
@@ -0,0 +1,19 @@
+function createStore () {
+    // The store should have four parts
+    // 1. The state
+    // 2. Get the state.
+    // 3. Listen to changes on the state.
+    // 4. Update the state
+  
+    let state
+    //new arrow  function
+    //it's responsible for returning the state  
+    const getState = () => state
+    
+
+    //when ever createstore is invoked we want to return an object
+    //the object returned has the ability to get the state
+    return {
+      getState
+    }
+  }
