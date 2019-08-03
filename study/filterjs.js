03307483b1abd20c053932d7a4f026fc211f79bc
ahmed tawfik <ahmedtaaw@gmail.com>
2019-08-03 16:01:49 +0200

    JS: Filter Native is odd

---
+++ study/filterjs.js
@@ -0,0 +1,15 @@
+//odd number 1, 3, 5, 7
+function isOdd(v){return v%2==1;}
+
+function exclude(arr,fn){
+    var list=[];
+    
+    for(var i=0;i<arr.length;i++){
+        if(fn(arr[i])){
+            list.push(arr[i])
+        }
+    }
+    return list;
+}
+
+console.log(exclude([1,2,3,4,5,6,7,8,9],isOdd))
