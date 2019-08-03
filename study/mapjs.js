306f624645fe1b7082c58d2e435116174aeb09ae
ahmed tawfik <ahmedtaaw@gmail.com>
2019-08-03 15:48:54 +0200

    JS: map usage examples

--- study/mapjs.js
+++ study/mapjs.js
@@ -3,3 +3,13 @@ function doubleIt(val){
 }
 
 console.log([1,2,3,4,5].map(doubleIt))
+
+/**
+ * example of using map
+ * -from lower to upper case
+ * -translations: list of strings to their translation into forgien language
+ * -list of string and trancating them into a certain length
+ * -prefix and sufix
+ * -list of functions, array that have a bunch of functions
+ * -list of functions and mapping them to their return or recall this function
+ */
