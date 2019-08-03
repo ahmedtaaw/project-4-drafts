53c9b67eee1c2841425553dd10c2d17b9cb6e1e8
ahmed tawfik <ahmedtaaw@gmail.com>
2019-08-03 16:05:18 +0200

    JS: Filter

--- study/filterjs.js
+++ study/filterjs.js
@@ -13,3 +13,9 @@ function exclude(arr,fn){
 }
 
 console.log(exclude([1,2,3,4,5,6,7,8,9],isOdd))
+
+function onlyOdds (val){
+    return val % 2 == 1;
+}
+
+console.log ([1,2,3,4,5,6,7,8,9].filter(onlyOdds))
