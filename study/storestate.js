e187a5039bf5642b8a138f1a17a65ba3d162aecc
ahmed tawfik <ahmedtaaw@gmail.com>
2019-08-30 17:32:05 +0400

    a way to listen to the state

--- study/storestate.js
+++ study/storestate.js
@@ -6,12 +6,21 @@ In this screencast, we started building out the createStore function. Currently,
     sets up a local (private) variable to hold the state
     sets up a getState() function
     returns an object that publicly exposes the getState() function
-
+    ---
+Thinking about the code we just wrote, what does the getState() function do?
+*it returns the existing state variable
 */
     let state
+    let listeners=[]
+
     const getState=()=>state
 
+    const subscribe = (listener)=>{
+        listeners.push(listener)
+    }
+
     return{
-        getState
+        getState,
+        subscribe
     }
 }
